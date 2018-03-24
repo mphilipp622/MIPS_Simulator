b21ac772d24e678dfaf1ba8f9e6044efaf645d2b
Mark Philipp <Wolfsenshi@gmail.com>
2018-03-24 09:37:47 -0700

    Setup Parser Skeleton

---
+++ MIPS Simulator/MIPS Simulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MIPS_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Parser newParser = new Parser("E:\\College\\MIPS Project\\MIPSSimulator\\MIPS Simulator\\MIPS Simulator\\Test.txt");
+			for (int i = 0; i < newParser.GetLines().Count; i++)
+				Console.WriteLine(newParser.GetLine());
+			//Parser.PrintHelloWorld();
+			//Console.WriteLine("Hello World!");
+		}
+    }
+}
